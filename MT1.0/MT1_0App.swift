fb61f7e6a1e407b753b96d0e5b25a168ce6667b2
Joseph DeWeese <rangerboy901@icloud.com>
2022-09-03 14:29:53 -0500

    Initial Commit

---
+++ MT1.0/MT1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  MT1_0App.swift
+//  MT1.0
+//
+//  Created by Joseph Wil;liam DeWeese on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MT1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
